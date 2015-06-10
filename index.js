9eca0c477af0983127890b51dfee16962086d835
Erin Call <hello@erincall.com>
2015-06-10 15:57:14 -0700

    Contact book app using jQuery and raw Node

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var fs = require('fs');
+var querystring = require('querystring');
 var path = require('path');
 var url = require('url');
 var http = require('http');
@@ -31,6 +32,12 @@ function determineMediaType(filename) {
 }
 
 /*
+The "database." A list of users; their "id" is their index into this array. Deleting a user
+means setting their entry to null, so as not to re-index the list (since the page JS can know about indices).
+*/
+var contacts = [];
+
+/*
 Given a filename, serve it from the public/ folder. Attempt to infer its media-
 type based on its extension. Sends headers and body and calls end, so don't do
 anything with the response before or after sending it to this function.
@@ -53,10 +60,142 @@ function serveFile(filename, response) {
   });
 }
 
+/*
+This is the main entry point for all API calls. It will look at the request
+and delegate to one of the individual handlers.
+*/
+function handleAPICall(apiPath, request, response) {
+  if (request.method.toUpperCase() === 'POST' && apiPath.match(/^people$/)) {
+    addUser(apiPath, request, response);
+  } else if (request.method.toUpperCase() === 'PATCH' && apiPath.match(/^people\/\d+$/)) {
+    updateUser(apiPath, request, response);
+  } else if (request.method.toUpperCase() === 'GET' && apiPath.match(/^people$/)) {
+    listUsers(apiPath, request, response);
+  } else if (request.method.toUpperCase() === 'DELETE' && apiPath.match(/^people\/\d+$/)) {
+    deleteUser(apiPath, request, response);
+  } else {
+    sendError(response, "Didn't understand your request!");
+  }
+}
+
+/*
+Add a new user to the database. Read the request body, then parse it with the built-in
+querystring module. Do some basic validation (age must be a number-like thing), then
+respond with JSON indicating success.
+*/
+function addUser(apiPath, request, response) {
+  var body = '';
+  request.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  request.on('end', function() {
+    body = querystring.parse(body);
+    var index = contacts.length,
+        responseBody;
+
+    if (isNaN(body.age)) {
+      sendError(response, "Age must be a number (it's *just* a number, after all)");
+    } else {
+      contacts[index] = body;
+      responseBody = JSON.stringify({status: 'ok', index: index});
+      response.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': responseBody.length});
+      response.write(responseBody);
+      response.end();
+    }
+  });
+}
+
+/*
+Update a user. Reads the request body just like addUser does, and does the same validation on age.
+Also checks to see that it's getting a valid user-index.
+*/
+function updateUser(apiPath, request, response) {
+  var body = '';
+  request.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  request.on('end', function() {
+    var index = Number(apiPath.replace('people/', '')),
+        responseBody;
+    body = querystring.parse(body);
+
+    if (contacts[index] === undefined || contacts[index] === null) {
+      sendError(response, "No such user :" + index);
+    } else if (isNaN(body.age)) {
+      sendError(response, "Age must be a number (it's *just* a number, after all)");
+    } else {
+      contacts[index] = body;
+      responseBody = JSON.stringify({status: 'ok', index: index});
+      response.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': responseBody.length});
+      response.write(responseBody);
+      response.end();
+    }
+  });
+}
+
+/*
+Return a list of all the contacts. JSON.stringify is sufficient here, since the "database" is just an in-memory
+array. Notice that the page JS is responsible for filtering out null entries, so that it gets the right indices.
+Suppose our contacts looked like
+  [
+    {"name": "Alice"},
+    null,
+    {"name": "Charles"}
+  ]
+
+If we filtered out nulls here, when the page received the list of users, it would think Charles's index was 2,
+instead of 3, so updates to Charles would fail.
+*/
+function listUsers(apiPath, request, response) {
+  var responseBody = JSON.stringify(contacts);
+  response.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': responseBody.length});
+  response.write(responseBody);
+  response.end();
+}
+
+/*
+Delete the given user by ID. Deleting in this case means replacing with null, so as to avoid reindexing.
+The request body doesn't have anything to tell us; we're just interested in the user-index on the url.
+*/
+function deleteUser(apiPath, request, response) {
+  var responseBody,
+      index = Number(apiPath.replace('people/', ''));
+
+  if (contacts[index] === undefined || contacts[index] === null) {
+    sendError(response, "No such user :" + index);
+  } else {
+    contacts[index] = null;
+      responseBody = JSON.stringify({status: 'ok'});
+      response.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': responseBody.length});
+      response.write(responseBody);
+      response.end();
+  }
+}
+
+/*
+Helper method for sending back API errors. It sends a generic HTTP 400 BAD REQUEST, with some json
+whose "error" attribute indicates the problem.
+*/
+function sendError(response, message) {
+  var responseBody = JSON.stringify({status: 'error', error: message});
+  response.writeHead(400, {'Content-Type': 'application/json', 'Content-Length': responseBody.length});
+  response.write(responseBody);
+  response.end();
+}
+
+/*
+The main request listener. Requests for / get the index.html that lives in public/.
+Requests for /api/a/n/y/t/h/i/n/g/ get sent on to handleAPICall.
+For any other request, try to serve it as a file in public/.
+*/
 server.on('request', function(request, response) {
   var pathname = url.parse(request.url).pathname;
   if (pathname === '/') {
     serveFile('index.html', response);
+  } else if (pathname.slice(0, 5) === '/api/') {
+    handleAPICall(pathname.slice(5), request, response);
   } else {
     // fs.exists takes a filename and calls a callback with a boolean
     // indicating whether that file exists.
--- public/js/page.js
+++ public/js/page.js
@@ -1,3 +1,144 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('all systems go!');
+/* jshint browser:true, jquery:true */
+'use strict';
+$(function() {
+  /*
+  Update a user record. This function assumes the attribute inputs on the page go [name, email, age], and that
+  they're of type email or text.
+  After collecting the needed information, it sets all the inputs in the form to "disabled" until the response
+  comes back. If there's an error, it attaches a new div containing the error text.
+  */
+  function updateUser(event) {
+    event.preventDefault();
+    var form = $(event.target),
+        inputs = form.find('input[type="text"], input[type="email"]'),
+        values = inputs.map(function() {return this.value;}),
+        name = values[0],
+        email = values[1],
+        age = values[2];
+
+    form.find('input').attr('disabled', true);
+
+    $.ajax(form.attr('action'), {
+      method: 'PATCH',
+      data: {
+        name: name,
+        email: email,
+        age: age
+      },
+      success: function() {
+        form.parent('div').find('.error').remove();
+        form.find('input').attr('disabled', false);
+      }, error: function(response) {
+        var errorMessage = $('<div class="error">');
+        errorMessage.text(response.responseJSON.error);
+        form.find('input').attr('disabled', false);
+        form.parent('div').find('.error').remove();
+        form.parent('div').prepend(errorMessage);
+      }
+    });
+  }
+
+  /*
+  Given information about a user, create a new row for that user and insert it into the contact list.
+  The large blob of $(html) calls could also look like:
+  <div class="row">
+    <form action="/api/people/{{index}}">
+      <label for="name-{{index}}">Name:</label>
+      <input type="text" name="name-{{index}}" id="name-{{index}}">
+      <label for="email-{{index}}">Email:</label>
+      <input type="email" name="email-{{index}}" id="email-{{index}}">
+      <label for="age-{{index}}">Age:</label>
+      <input type="text" name="age-{{index}}" id="age-{{index}}">
+      <input type="submit" value="Update">
+      <button>Delete</button>
+    </form>
+  </div>
+
+  Submitting the form triggers the deleteUser function above. Clicking the "delete" button
+  sends a DELETE request, and on success, removes the whole row.
+
+  The newly-created row goes just above the "add new" row.
+  */
+  function appendContact(name, email, age, index) {
+    var entry = $('<div class="row">'),
+        form = $('<form action="/api/people/' + index + '">'),
+        nameLabel = $('<label for="name-' + index + '">Name: </label>'),
+        nameInput = $('<input type="text" name="name-' + index + '" id="name-' + index + '">'),
+        emailLabel = $('<label for="email-' + index + '">Email: </label>'),
+        emailInput = $('<input type="email" name="email-' + index + '" id="email-' + index + '">'),
+        ageLabel = $('<label for="age-' + index + '">Age: </label>'),
+        ageInput = $('<input type="text" name="age-' + index + '" id="age-' + index + '">'),
+        updateButton = $('<input type="submit" value="Update">'),
+        deleteButton = $('<button>Delete</button>');
+
+    deleteButton.click(function(event) {
+      event.preventDefault();
+      $.ajax(form.attr('action'), {
+        method: 'DELETE',
+        success: function() {
+          entry.remove();
+        }
+      });
+    });
+
+    nameInput.val(name);
+    emailInput.val(email);
+    ageInput.val(age);
+
+    form.append(nameLabel, nameInput, emailLabel, emailInput, ageLabel, ageInput, updateButton, deleteButton);
+    form.submit(updateUser);
+    entry.append(form);
+
+    $('.add-new').before(entry);
+  }
+
+  /*
+  Submit event handler for the "add new contact" form. It gets the needed information from the form fields,
+  then makes a POST request to create a new contact. When the response comes back with the new contact's id,
+  pass all the gathered information, along with the new id, to appendContact.
+  */
+  $('.add-new form').submit(function(event) {
+    event.preventDefault();
+    var form = $(event.target),
+        name = $('#name').val(),
+        email = $('#email').val(),
+        age = $('#age').val();
+    form.attr('disabled', true);
+    $.ajax('/api/people', {
+      method: 'POST',
+      data: {
+        name: name,
+        email: email,
+        age: age
+      },
+      success: function(response) {
+        appendContact(name, email, age, response.index);
+        $('#name, #email, #age').val('');
+        form.attr('disabled', false);
+        $('.add-new').find('.error').remove();
+      }, error: function(response) {
+        var errorMessage = $('<div class="error">');
+        errorMessage.text(response.responseJSON.error);
+        $('.add-new').find('.error').remove();
+        $('.add-new').prepend(errorMessage);
+        form.attr('disabled', false);
+      }
+    });
+  });
+
+  /*
+  On loading the page, fetch all the previously-created contacts.
+  Pass them along to appendContact, filtering out nulls.
+  We have to filter out nulls because of the crappy way the server-side code handles indexing.
+  */
+  $.ajax('/api/people', {
+    method: 'GET',
+    success: function(response) {
+      response.forEach(function(contact, index) {
+        if (contact !== null) {
+          appendContact(contact.name, contact.email, contact.age, index);
+        }
+      });
+    }
+  });
 });
